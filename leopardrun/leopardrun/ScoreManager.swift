29bc704a198b9ba0908225ca672ea463f30a290d
Jonathan <Jonathan@nosfair.com>
2015-05-21 10:22:45 +0200

    managers are singelton classes

--- leopardrun/leopardrun/ScoreManager.swift
+++ leopardrun/leopardrun/ScoreManager.swift
@@ -9,7 +9,18 @@
 import Foundation
 
 class ScoreManager {
-    
+     
+    class var sharedInstance: ScoreManager {
+        struct Static {
+            static var onceToken: dispatch_once_t = 0
+            static var instance: ScoreManager? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = ScoreManager()
+        }
+        return Static.instance!
+    }
+
     init()
     {
         
