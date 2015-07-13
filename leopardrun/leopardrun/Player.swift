d5181d0a4cc767260ad669435f8e2eeea67bce87
Felix-André Böttger <Ich>
2015-07-13 15:00:54 +0200

    + comments weg!

--- leopardrun/leopardrun/Player.swift
+++ leopardrun/leopardrun/Player.swift
@@ -38,11 +38,6 @@ class Player: SpriteEntity {
             physics.dynamic = true
             physics.mass = 0.1
             physics.density = 1
-
-            planet.physicsBody.categoryBitMask = planetCategory;
-            planet.physicsBody.collisionBitMask = planetCategory | edgeCategory;
-            planet.physicsBody.contactTestBitMask = 0;
-            
             
             physics.categoryBitMask = BodyType.player.rawValue
             physics.collisionBitMask = BodyType.box.rawValue | BodyType.ground.rawValue | BodyType.item.rawValue | BodyType.sky.rawValue
